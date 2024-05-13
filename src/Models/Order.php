3780a6fcd0955d825c01495a30be9793ed14c3c0
vladyslavlopatin <ilopatin3@gmail.com>
2024-05-13 23:12:28 +0200

    ->withTrashed() user

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -29,7 +29,8 @@ class Order extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)
+            ->withTrashed();
     }
 
     public function product(): BelongsTo
