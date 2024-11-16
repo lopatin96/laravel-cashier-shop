ccd66713998f90f66ece1a0ed1edfca24c5bb1e0
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-16 18:57:33 +0100

    js local

---
+++ lang/ja/common.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'Shop | :app_name' => ':app_name | ショップ',
+
+    'Shop' => 'ショップ',
+    'Signed in as' => 'サインイン済み：',
+    'Purchase management for' => '購入管理：',
+    'sidebar-text' => '制限を解除し、機能を次のレベルに引き上げる製品を購入しましょう。',
+    'Return to' => '戻る',
+
+    'info-title' => '製品を購入',
+    'info-text-0' => '<p>以下は、個別に購入できるすべての製品のリストです。また、<a href="/billing" target="_blank" class="underline hover:no-underline">月額プランでお得な料金に加入</a>することもできます。</p>',
+    'info-text-1' => '<p>以下は、個別に購入できるすべての製品のリストです。</p>',
+
+    'alerts.success' => '支払いが成功しました。製品を購入しました。<a href="/dashboard" class="underline hover:no-underline">サービスに戻る</a>か、ショップにとどまります。',
+
+    'product.quantity.more.tooltip' => 'もう1つ',
+    'product.quantity.less.tooltip' => '1つ減らす',
+
+    'order-history.title' => '注文履歴',
+    'order-history.product' => '製品',
+    'order-history.quantity' => '数量',
+    'order-history.date' => '日付',
+
+    'buttons.buy.title' => '今すぐ購入'
+
+];
