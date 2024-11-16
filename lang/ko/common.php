629ed9fc0c7a33e0ef56a57758bb6c3f21f4dba8
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-17 00:36:10 +0100

    ko local

---
+++ lang/ko/common.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'Shop | :app_name' => 'Shop | :app_name',
+
+    'Shop' => '상점',
+    'Signed in as' => '로그인한 사용자',
+    'Purchase management for' => '구매 관리',
+    'sidebar-text' => '제한을 제거하고 능력을 새로운 차원으로 끌어올릴 수 있는 제품을 구매하세요.',
+    'Return to' => '돌아가기',
+
+    'info-title' => '제품 구매',
+    'info-text-0' => '<p>아래는 개별적으로 구매할 수 있는 모든 제품 목록입니다. 또한 <a href="/billing" target="_blank" class="underline hover:no-underline">월간 요금제를 통해 더 좋은 요금을 구독</a>할 수 있습니다.</p>',
+    'info-text-1' => '<p>아래는 개별적으로 구매할 수 있는 모든 제품 목록입니다.</p>',
+
+    'alerts.success' => '결제가 성공적으로 완료되었습니다. 제품을 구매하셨습니다. <a href="/dashboard" class="underline hover:no-underline">서비스로 돌아가</a>거나 상점에 계속 있을 수 있습니다.',
+
+    'product.quantity.more.tooltip' => '하나 더',
+    'product.quantity.less.tooltip' => '하나 덜',
+
+    'order-history.title' => '주문 내역',
+    'order-history.product' => '제품',
+    'order-history.quantity' => '수량',
+    'order-history.date' => '날짜',
+
+    'buttons.buy.title' => '지금 구매',
+
+];
