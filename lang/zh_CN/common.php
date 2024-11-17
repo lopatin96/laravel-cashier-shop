2cde44a5adf3e72f00e239ef0a39603a71401e28
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-17 01:13:23 +0100

    zh_CN local

---
+++ lang/zh_CN/common.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'Shop | :app_name' => '商店 | :app_name',
+
+    'Shop' => '商店',
+    'Signed in as' => '已登录为',
+    'Purchase management for' => '购买管理',
+    'sidebar-text' => '购买产品，解除限制，让您的能力迈上一个全新的台阶。',
+    'Return to' => '返回',
+
+    'info-title' => '购买产品',
+    'info-text-0' => '<p>以下是您可以单独购买的所有产品列表。您还可以通过我们的月度计划 <a href="/billing" target="_blank" class="underline hover:no-underline">订阅以获得更优惠的价格</a>。</p>',
+    'info-text-1' => '<p>以下是您可以单独购买的所有产品列表。</p>',
+
+    'alerts.success' => '支付成功，您已购买该产品。您可以 <a href="/dashboard" class="underline hover:no-underline">返回服务</a> 或继续留在商店。',
+
+    'product.quantity.more.tooltip' => '再加一个',
+    'product.quantity.less.tooltip' => '少一个',
+
+    'order-history.title' => '订单历史',
+    'order-history.product' => '产品',
+    'order-history.quantity' => '数量',
+    'order-history.date' => '日期',
+
+    'buttons.buy.title' => '立即购买',
+
+];
---
+++ lang/zh_CN/specific.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'categories.[XXX].title' => '',
+
+    'products.[XXX].[YYY].title' => '',
+    'products.[XXX].[YYY].description' => '',
+    'products.[XXX].[YYY].subtitle' => '',
+
+
+];
