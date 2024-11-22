bbf884509534c7c6705695847aa0884d6f2c6a4b
Vladyslav Lopatin <lopatin@wskz.pl>
2024-11-22 14:42:12 +0100

    price in notifications

--- src/Notifications/NewOrder.php
+++ src/Notifications/NewOrder.php
@@ -26,12 +26,12 @@ class NewOrder extends Notification implements ShouldQueue
     {
         $additionalUserInfo = '';
 
-        if ($this->order->user->locale) {
-            $additionalUserInfo .= $additionalUserInfo ? ' · '.$this->order->user->locale : $this->order->user->locale;
-        }
-
         if ($this->order->user->country) {
             $additionalUserInfo .= $additionalUserInfo ? ' · '.$this->order->user->country : $this->order->user->country;
+        }
+
+        if ($this->order->user->locale) {
+            $additionalUserInfo .= $additionalUserInfo ? ' · '.$this->order->user->locale : $this->order->user->locale;
         }
 
         if ($this->order->user->device) {
@@ -43,7 +43,7 @@ class NewOrder extends Notification implements ShouldQueue
             ->line((app()->isProduction() ? '' : 'TEST ').'*[New Order]*')
             ->line('_ID:_ '.$this->order->id)
             ->line('_Product:_ '.$this->order->product->name)
-            ->line('_Status:_ '.$this->order->product->status->value)
+            ->line('Price:_ '.$this->order->product->getDisplayPrice())
             ->line('_Quantity:_ '.$this->order->quantity)
             ->line('_User ID:_ '.$this->order->user->id)
             ->line('_User Email:_ '.$this->order->user->email)
