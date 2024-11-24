b13786490532d11502e8057ea78f1b81cdadfcf7
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-24 22:57:47 +0100

    getTotalProcessedOrderCount and getTotalProcessedOrderAmountInCents

--- src/Traits/HasOrders.php
+++ src/Traits/HasOrders.php
@@ -2,6 +2,7 @@
 
 namespace Atin\LaravelCashierShop\Traits;
 
+use Atin\LaravelCashierShop\Enums\OrderStatus;
 use Atin\LaravelCashierShop\Models\Order;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -13,13 +14,21 @@ trait HasOrders
             ->withTrashed();
     }
 
-    public function getTotalOrderCount(): int
+    public function getTotalProcessedOrderCount(): int
     {
-        return $this->orders()->count();
+        return $this->orders->where('status', OrderStatus::Processed)->count();
     }
 
-    public function getTotalOrderAmount(): int
+    public function getTotalProcessedOrderAmountInCents(): int
     {
-        return $this->orders()->count();
+        $amountInCents = 0;
+
+        foreach($this->orders->where('status', OrderStatus::Processed) as $order) {
+            if (property_exists($order->log, 'amount') && property_exists($order->log, 'currency')) {
+                $amountInCents += $order->log->amount / config('laravel-cashier-shop.exchange_rates_for_usd')[$order->log->currency];
+            }
+        }
+
+        return $amountInCents;
     }
 }
