a0393827c2bc3bc501105ffa3b0ed7d7d4b736f0
vladyslavlopatin <ilopatin3@gmail.com>
2024-12-11 18:35:16 +0100

    convertAmount

--- src/Helpers/CurrencyHelper.php
+++ src/Helpers/CurrencyHelper.php
@@ -3,6 +3,7 @@
 namespace Atin\LaravelCashierShop\Helpers;
 
 use Atin\LaravelCashierShop\Models\Currency;
+use InvalidArgumentException;
 
 class CurrencyHelper
 {
@@ -10,5 +11,18 @@ class CurrencyHelper
     {
         return Currency::whereJsonContains('country_codes', $countryCode)->first()
             ?? Currency::where('iso_code', 'USD')->first();
+    }
+
+    public static function convertAmount(int $amount, string $fromCurrency, string $toCurrency): int
+    {
+        $exchangeRates = config('laravel-cashier-shop.exchange_rates_for_usd');
+
+        if (! isset($exchangeRates[$fromCurrency], $exchangeRates[$toCurrency])) {
+            throw new InvalidArgumentException('Exchange rate for one or both currencies is not defined.');
+        }
+
+        $amountInUsd = $amount / $exchangeRates[$fromCurrency];
+
+        return $amountInUsd * $exchangeRates[$toCurrency];
     }
 }
